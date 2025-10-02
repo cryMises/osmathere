8e7f6f44ad7e1aa6a0eb05e441ba09458d1a4f82
cryMises <samuel314285@gmail.com>
2025-10-02 10:46:14 +0700

    Update script.js

--- script/script.js
+++ script/script.js
@@ -253,7 +253,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
 
 // Set the date we're counting down to
-var countDownDate = new Date("Oct 5, 2025 23:59:59").getTime();
+var countDownDate = new Date("Oct 4, 2025 23:59:59").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
